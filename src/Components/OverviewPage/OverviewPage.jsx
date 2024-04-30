94697eaf7b97b6b30333df96d80303d48dffbe1f
Sehaj Naangal <snaangal@uwo.ca>
2024-04-29 21:58:02 -0400

    add styling to team section

--- src/Components/OverviewPage/OverviewPage.jsx
+++ src/Components/OverviewPage/OverviewPage.jsx
@@ -8,14 +8,6 @@ const OverviewPage = () => {
   return (
     <div className={styles['overview-container']}>
       <div className={styles['overview-container-2']}>
-        <div className={`${styles['section-title-text-container']} ${styles['video-text-container']}`}>
-          <div className={styles['horizontal-line']}></div>
-          <div className={styles['section-title-text']}>Video</div>
-          <div className={styles['horizontal-line']}></div>
-        </div>
-        <div className={styles['video']}>
-          <YoutubeEmbed embedId="XIxxdxKqV5I" />
-        </div>
         <div className={`${styles['section-title-text-container']} ${styles['photo-text-container']}`}>
           <div className={styles['horizontal-line']}></div>
           <div className={styles['section-title-text']}>Gallery</div>
@@ -24,19 +16,29 @@ const OverviewPage = () => {
         <div className={styles['carousel']}>
           <ImageCarousel />
         </div>
+        <div className={`${styles['section-title-text-container']} ${styles['video-text-container']}`}>
+          <div className={styles['horizontal-line']}></div>
+          <div className={styles['section-title-text']}>Video</div>
+          <div className={styles['horizontal-line']}></div>
+        </div>
+        <div className={styles['video']}>
+          <YoutubeEmbed embedId="XIxxdxKqV5I" />
+        </div>
         <div className={`${styles['section-title-text-container']} ${styles['team-text-container']}`}>
           <div className={styles['horizontal-line']}></div>
           <div className={styles['section-title-text']}>Team</div>
           <div className={styles['horizontal-line']}></div>
         </div>
-        <div className={styles['team-container']}>
-          <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
-          <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
-          <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
-          <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
-          <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
-          <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
-          <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
+        <div className={styles['team-outer-container']}>
+          <div className={styles['team-container']}>
+            <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
+            <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
+            <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
+            <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
+            <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
+            <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
+            <TeamMember name={'Sehaj Naangal'} position={'Front-End Developer'} linkedInUsername={'sehajnaangal'} />
+          </div>
         </div>
       </div>
     </div >
